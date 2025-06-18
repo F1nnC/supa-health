051464e47f2174a258bbefff066deb691a08d51b
F1nnC <captainsurfervideos@gmail.com>
2025-06-17 17:18:19 -0700

    wait ok innit

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
 import Image from "next/image";
 
-import { supabase } from '../lib/supabaseClient'
-
 export default function Home() {
 
-  async function test() {
-    const { data, error } = await supabase.from('habits').select('*')
-    console.log({ data, error })
-  }
-
+ 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
